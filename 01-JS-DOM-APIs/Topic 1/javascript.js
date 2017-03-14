0f262e65538392f62f857ba742cd77d30031a192
ximef <ximenafuertes@gmail.com>
2017-03-14 10:38:59 -0300

    topic 1 - 7

--- 01-JS-DOM-APIs/Topic 1/javascript.js
+++ 01-JS-DOM-APIs/Topic 1/javascript.js
@@ -1,58 +1,28 @@
 function setTrue(){
 	document.getElementById('test').hidden = true;
-};
+}
 
 function hide(){
     document.getElementById('test').hidden = false;
 	setTimeout("setTrue()",3000);
-};
-
-function loadXMLDoc() {
-  var xmlhttp;
-  if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    // code for older browsers
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      callback(xmlhttp.responseText);
-    }
-  };
-
-  function callback(respString) {
-            var joke = getJoke(respString);
-            var quoteList = document.getElementById("joke").innerHTML = joke;
 }
 
-function getJoke(respString) {
-    var obj = JSON.parse(respString);
-    return obj.value.joke;
-}
+function getData(){
 
-  xmlhttp.open("GET", "http://api.icndb.com/jokes/random", true);
-  xmlhttp.send();
-};
-
-// We removed the callback parameter cause we don't need it anymore
-function getData(url) {
-    
-    // Return a new promise and wrap the previous logic into the anonymous function
-    return new Promise(function(callback, reject) {
+    var promise = new Promise(function(resolve, reject) {
+        
         var req = new XMLHttpRequest();
     
-        req.open("GET", url);
+        req.open("GET", "http://api.icndb.com/jokes/random");
         
-        req.onreadystatechange = function() {
-            if (req.status !== 200) {
+        req.onload = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               resolve(req.responseText);
+            }
+            else {
                 // Instead of the callback we're calling the reject callback of the promise
                 reject(new Error("Status code wasn't 200"));
-                return;
             }
-            
-            callback(req.responseText);
         }
         
         req.onerror = function() {
@@ -62,12 +32,14 @@ function getData(url) {
         
         req.send();
     });
-    
-}
 
-// A typical call looks like this
-getData('product.json').then(function(result) {
-    console.log('Success!', result)
-}, function(err) {
-    console.error('Failed!', err);
-});
+  promise.then(function(data){
+    console.log('Got data! Promise fulfilled.');
+    document.getElementById('joke').innerHTML = JSON.parse(data).value.joke;
+  }, function(error){
+    console.log('Promise rejected.');
+    console.log(error.message);
+    document.getElementById("joke").innerHTML = "Error";
+  });
+
+}
